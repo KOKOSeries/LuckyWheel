572b63578d4b71f81ab5f89696ebcb70735bd700
Stark Stark <gladish0606@gmail.com>
2020-12-07 18:17:27 +0700

    commit22

--- LuckyLottery/Pods/Target Support Files/Pods-LuckyLottery-LuckyLotteryUITests/Pods-LuckyLottery-LuckyLotteryUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LuckyLottery_LuckyLotteryUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LuckyLottery_LuckyLotteryUITests
-@end
--- LuckyLottery/Pods/Target Support Files/Pods-LuckyLottery/Pods-LuckyLottery-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LuckyLottery : NSObject
-@end
-@implementation PodsDummy_Pods_LuckyLottery
-@end
